ea22327ff12b1335de86c6e23c82b38bb26b805b
ieu_vannn <127402552+galvanam@users.noreply.github.com>
2023-10-26 08:23:05 +0700

    Add files via upload

---
+++ pages/contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function contact() {
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <p>Feel free to contact us for any inquiries.</p>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function home() {
+  return (
+    <div>
+      <h2>INI HOME</h2>
+      <ul>
+        <li>Info 1</li>
+        <li>Info 11</li>
+        <li>Info 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
